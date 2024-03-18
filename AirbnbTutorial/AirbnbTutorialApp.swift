2c647be5e9934c7a376ad0da41628c4b699f85e8
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-03-18 15:28:45 +0100

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Sidney MALEO on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
