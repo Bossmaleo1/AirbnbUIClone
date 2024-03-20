60ead12c7abbaf81839fbcd86bbd7a86327887b3
Bossmaleo1 <bossprogrammaleo@gmail.com>
2024-03-20 17:11:12 +0100

    we add our project init

---
+++ AirbnbTutorial/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,93 @@
+//
+//  DestinationSearchView.swift
+//  AirbnbTutorial
+//
+//  Created by Sidney MALEO on 20/03/2024.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    @Binding var show: Bool
+    @State private var destination = ""
+    
+    var body: some View {
+        VStack {
+            
+            Button {
+                withAnimation(.snappy) {
+                    show.toggle()
+                }
+            } label: {
+                Image(systemName: "xmark.circle")
+                    .imageScale(.large)
+                    .foregroundStyle(.black)
+                
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .imageScale(.small)
+                    
+                    TextField("Search destination", text: $destination)
+                        .font(.subheadline)
+                }
+                .frame(height: 44)
+                .padding(.horizontal)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(lineWidth: 1.0)
+                        .foregroundStyle(Color(.systemGray4))
+                    
+                }
+            }
+            .padding()
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding()
+            .shadow(radius: 10)
+            
+            // date selection view
+            CollapsedPickerView(title: "when", description: "Add dates")
+            
+            // num guests view
+            CollapsedPickerView(title: "Whi", description: "Add guests")
+            
+        }
+    }
+}
+
+#Preview {
+    DestinationSearchView(show: .constant(false))
+}
+
+struct CollapsedPickerView: View {
+    let title: String
+    let description: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text(title)
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Text(description)
+            }
+            .fontWeight(.semibold)
+            .font(.subheadline)
+            
+        }
+        .padding()
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding()
+        .shadow(radius: 10)
+    }
+}
